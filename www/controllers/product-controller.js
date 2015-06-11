fc523bfda8c4a7ae3b76515881f1c99006061b0d
emiovska <miovska.e@gmail.com>
2015-06-11 22:46:53 +0200

    Barcode design changed

--- www/controllers/product-controller.js
+++ www/controllers/product-controller.js
@@ -2,17 +2,22 @@ app.controller('ProductController', ['$scope','D2RService','$cordovaBarcodeScann
 function($scope,D2RService,$cordovaBarcodeScanner,$ionicPopup) {
 
   $scope.data = {
-    barcodeInput: ""
+    barcodeInput: "",
+    sensor: 0
   }
 
-  $("#barcode").JsBarcode("1234567891234",{width:1,displayValue:false,fontSize:20});
+  $scope.showBarcodeInput=true;
+
+  $("#barcode").JsBarcode("1234567891234",{width:1,displayValue:false,fontSize:20,lineColor:"#526666"});
   $scope.showSpinner=false;
 
   $scope.updateBarcode = function(val){
-      if(val.length>=13)
+      if(val.length>=13){
         $("#barcode").JsBarcode(val,{width:1,displayValue:true,fontSize:20});
-      else
-        $("#barcode").JsBarcode("1234567891234",{width:1,displayValue:false,fontSize:20});
+        $scope.showBarcodeInput=false;
+      }
+     else
+        $("#barcode").JsBarcode("1234567891234",{width:1,displayValue:false,fontSize:20,lineColor:"#526666"});
 
       $scope.showProductTable=false;
   };
@@ -41,12 +46,13 @@ function($scope,D2RService,$cordovaBarcodeScanner,$ionicPopup) {
     });
   }
 
-  $scope.scanBarcode = function(){
+  var scanBarcode = function(){
      $cordovaBarcodeScanner
           .scan()
           .then(function(barcodeData) {
             $scope.data = {
-              barcodeInput: barcodeData.text
+              barcodeInput: barcodeData.text,
+              sensor: 0
             };
             $scope.updateBarcode(barcodeData.text);
           },function(error) {
@@ -54,6 +60,17 @@ function($scope,D2RService,$cordovaBarcodeScanner,$ionicPopup) {
           });
   }
 
+
+  $scope.editBarcode = function() {
+    $scope.showBarcodeInput=true;
+    $("#barcode").JsBarcode("1234567891234",{width:1,displayValue:false,fontSize:20,lineColor:"#526666"});
+  };
+
+  $scope.sensorDetection = function(sensor) {
+    if(sensor>=100){
+        scanBarcode();
+    }
+  }
 //  $scope.parseXML=function(){
 //    var json = $.xml2json("<?xml version=\"1.0\"?> <sparql xmlns=\"http://www.w3.org/2005/sparql-results#\"> <head> <variable name=\"brandName\"/> <variable name=\"brandUrl\"/> <variable name=\"ownerName\"/> <variable name=\"wikiLink\"/> </head> <results> <result> <binding name=\"brandName\"> <literal>Fanta</literal> </binding> <binding name=\"brandUrl\"> <literal>http://www.fanta.com</literal> </binding> <binding name=\"ownerName\"> <literal>Coca-Cola</literal> </binding> <binding name=\"wikiLink\"> <literal>http://en.wikipedia.org/wiki/The_Coca-Cola_Company</literal> </binding> </result> </results> </sparql>");
 //    console.log(json.results.result.binding);
